d64cb0e46d05e2d724977656cca9c0e587e6d871
Abdellah ALAOUI ISMAILI <abdellah.alaoui2006@gmail.com>
2025-04-11 19:13:09 +0100

    Update config.py

--- instance/config.py
+++ instance/config.py
@@ -1,6 +1,6 @@
 import os
 
 SECRET_KEY = os.urandom(24)
-STRIPE_API_KEY = 'SmFjb2IgS2FwbGFuLU1vc3MgaXMgYSBoZXJv'
-SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://admin_db:9UcZJxdB440TqguLfUYTQgh3I@localhost:3306/infralinker_db'
+STRIPE_API_KEY = 'gen_apikey_her'
+SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://admin_db:admin_password_her@localhost:3306/infralinker_db'
 SQLALCHEMY_POOL_RECYCLE = 60
